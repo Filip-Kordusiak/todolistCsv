836565c88def2227e99f0201dbe79e05cb24b303
kordixd <kordusiakfilip@gmail.com>
2022-06-29 20:02:26 +0200

    ToDoList, with csv and txt

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import *
 from tkinter import ttk
+import csv
 
 root = Tk()
+
+
 def ukonczone():
 
     w2.insert(0, w.get(w.curselection()))
     w.delete(w.curselection())
+
 
 frame = LabelFrame(root)
 frame.grid(row=0, column=0)
@@ -20,8 +24,23 @@ b4.grid(row=1, column=0)
 def dodaj():
     e.get()
     w.insert(0, e.get())
-def usun():
 
+
+def usun():
+    new_list = w2.get(first=0, last=END)
+    #print(new_list)
+    p = 0
+    for row in new_list:
+        if row == "zjeść":
+            p = p + 1
+        print(row)
+        print(p)
+    pp = "dzisiaj zjadłeś razy: % s" % p
+    with open('raport.txt', 'w') as csvfile1:
+        csvfile1.write(pp)
+    with open('plik.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(new_list)
     w2.delete(first=0, last=END)
 
 
@@ -42,5 +61,25 @@ w2 = Listbox(frame3)
 w2.grid(row=0, column=0)
 b3 = Button(frame3, text="usuń wszystko", command=usun)
 b3.grid(row=1, column=0)
+
+
+class Czwarta:
+    def __init__(self, x):
+        self.x = x
+        print(x)
+
+    def dodaj(self):
+        print("xddd")
+
+
+xd = Czwarta('czesc')
+frame4 = LabelFrame(root)
+frame4.grid(row=3, column=0)
+b4 = Button(frame4, text="xd", command=xd.dodaj)
+b4.grid(row=2, column=0)
+#####
+
+
+
 
 root.mainloop()
